eda5fcd51c21f609712669693656ca4600dfe6d6
lnarote777 <lnarote777@iesrafaelalberti.es>
2025-03-21 20:06:03 +0100

    carpeta api

---
+++ Api-Tfg/src/main/kotlin/com/example/Api_Tfg/ApiTfgApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Api_Tfg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiTfgApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiTfgApplication>(*args)
+}
